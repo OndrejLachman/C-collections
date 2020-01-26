e17dc92a95f89fb8e2ca5c96735d432e37eb86fd
Ondra Lachman <ondralachman48@gmail.com>
2020-01-26 20:53:23 +0100

    finished "seřazeno"

--- Serazeno/Serazeno/Serazeno/Program.cs
+++ Serazeno/Serazeno/Serazeno/Program.cs
@@ -38,37 +38,72 @@ namespace Serazeno
 
             LinkedList<string> Llist = new LinkedList<string>();
 
+            Llist.AddLast("ahoj");
+            Llist.AddLast("Jak se máš?");
+
+            //Nejsou seøazeny
+
             Console.WriteLine("\n---hashset---");
 
             HashSet<string> Hashset = new HashSet<string>();
 
+            Hashset.Add("Ahoj");
+            Hashset.Add("Jak se máš?");
+
+            //Nejsou seøazeny
             Console.WriteLine("\n---ConcurrentBag---");
 
             ConcurrentBag<string> cBag = new ConcurrentBag<string>();
+
+            //Nejsou seøazeny
 
             Console.WriteLine("\n---Sortedset---");
 
             SortedSet<string> Sset = new SortedSet<string>();
 
+            //øazeno abecednì
+
             Console.WriteLine("\n---Dictionary---");
 
-            Dictionary<string, string> dictionary = new Dictionary<string, string>();
 
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            dictionary.Add(1,"ahoj");
+            dictionary.Add(2, "Jak se máš?");
+
+            //Nejsou øazeny
             Console.WriteLine("\n---ConcurrentDictionary---");
 
             ConcurrentDictionary<int, string> Cdictionary = new ConcurrentDictionary<int, string>();
+
+            //neøazeno
 
             Console.WriteLine("\n---SortedDictionary---");
 
             SortedDictionary<int, string> sDictionary = new SortedDictionary<int, string>();
 
+            sDictionary.Add(2, "Ahoj");
+            sDictionary.Add(1, "Jak se máš?");
+
+            foreach (KeyValuePair<int, string> s in sDictionary)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    s.Key, s.Value);
+            }
+
+            // øazeno podle klíèe
+
             Console.WriteLine("\n---Queue---");
 
             Queue<string> queue = new Queue<string>();
 
+            //neøazeno
+    
             Console.WriteLine("\n---Stack---");
 
             Stack stack = new Stack();
+
+            //neøazeno
 
 
         }
